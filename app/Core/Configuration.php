5e2af18c1af0ebe28bb9fb2615cd34ccdf01f6f0
victor <victorhugoTI@outlook.com>
2020-05-26 14:01:45 -0300

    Criando classe de configuração, conexao

---
+++ app/Core/Configuration.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Core;
+
+abstract class Configuration
+{
+
+    protected const DB_HOST = '127.0.0.1';
+    protected const DB_USER = 'root';
+    protected const DB_PASS = '';
+    protected const DB_NAME = 'sistema_academia';
+    protected const DB_CHARSET = 'utf8';
+}
---
+++ app/Core/Database/Connection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Core\Database;
+
+use App\Core\Configuration;
+use PDO;
+use PDOException;
+
+class Connection extends Configuration
+{
+
+    private $host, $user, $pass, $dbname, $charset;
+
+    public function __construct()
+    {
+        $this->host = self::DB_HOST;
+        $this->user = self::DB_USER;
+        $this->pass = self::DB_PASS;
+        $this->dbname = self::DB_NAME;
+        $this->charset = self::DB_CHARSET;
+    }
+
+    public function connection()
+    {
+        try {
+            $pdo = new PDO("mysql:host={$this->host};dbname={$this->dbname};charset={$this->charset}", $this->user, $this->pass);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            return $pdo;
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+    }
+}
